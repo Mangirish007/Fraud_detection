c7c54a237698a17fc716031b780e3a980ce55bbf
Mangirish007 <mangirish.kavlekar21@gmail.com>
2022-02-16 10:49:02 +0530

    adding the changes

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,19 @@
 
 
 print("All the code for ML project")
+
+
+def model1():
+    print("model 1")
+
+
+def model2():
+    print("model 2")
+
+def model3():
+    print("model 2")
+
+def model4():
+    print("model 3")
+
+print("use perfect model2")
